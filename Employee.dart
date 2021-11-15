5e3882a22a72d008c96f9b87effdf5c0feb75698
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-11-15 01:23:46 -0800

    Added: Program To Calculate Daily Employee Wage

--- Employee.dart
+++ Employee.dart
@@ -2,14 +2,17 @@ import 'dart:math';
 
 class Employee {
   Random random = Random();
-  int result = 0;
-  final int min = 0, max = 2;
+  int result = 0, totalEmpWage = 0;
+  final int min = 0, max = 2, wagePerHours = 20;
   void checkEmployee() {
     result = min + random.nextInt(max - min);
     if (result == 0) {
       print('Employee is present');
+      totalEmpWage = wagePerHours * 8;
     } else {
       print('Employee is absent');
+      totalEmpWage = wagePerHours * 0;
     }
+    print('Total employee wage is : $totalEmpWage');
   }
 }
