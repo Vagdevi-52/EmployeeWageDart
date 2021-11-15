c386b85ca4ca0cef210f6edfd26209f376e54b3f
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-11-15 01:40:54 -0800

    Added: Program To Solve Using Switch Case Statement

--- Employee.dart
+++ Employee.dart
@@ -3,21 +3,33 @@ import 'dart:math';
 class Employee {
   Random random = Random();
   int result = 0, totalEmpWage = 0;
-  final int min = 0, max = 2, wagePerHours = 20;
+  final int min = 0, max = 3, wagePerHours = 20;
 
   void employeeWage() {
     result = min + random.nextInt(max - min);
-    if (result == 0) {
-      print('Employee is present');
-      totalEmpWage = wagePerHours * 8;
-    }
-    if (result == 1) {
-      print('Employee is  present only half day');
-      totalEmpWage = wagePerHours * 4;
-    }
-    if (result == 2) {
-      print('Employee is absent');
-      totalEmpWage = wagePerHours * 0;
+    switch (result) {
+      case 0:
+        {
+          print('Employee is present');
+          totalEmpWage = wagePerHours * 8;
+        }
+        break;
+      case 1:
+        {
+          print('Employee is  present half day only');
+          totalEmpWage = wagePerHours * 4;
+        }
+        break;
+      case 2:
+        {
+          print('Employee is absent');
+          totalEmpWage = wagePerHours * 0;
+        }
+        break;
+      default:
+        {
+          print('it is not a right choice');
+        }
     }
     print('Total employee wage is : $totalEmpWage');
   }
